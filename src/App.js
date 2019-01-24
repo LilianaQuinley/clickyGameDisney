e8377738649acb19fe65a789e97e366cf1b98234
liliana Quinley <lilicuellar99@gmail.com>
2019-01-24 01:02:02 -0700

    Final One

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 import React, { Component } from 'react';
+
 import './App.css';
 import { images } from './images.json';
 const shuffle = require('shuffle-array');
+
 
 //import Navigation from './components/Navigation'; 
 
@@ -10,28 +12,57 @@ class App extends Component {
   constructor (props){
     super(props);
     this.state = {
-      images
+      images,
+      clickedTwiceImages: [],
+      topScore: 0,
+      score:0,
+      title: "Click an Image to Begin!"     
     }
   }
 
-  imageClick =() => {
-    console.log("click Image");
-    shuffle(this.state.images);
-    this.forceUpdate();
-  
+  imageClick =(clickedImage) => {
+
+      shuffle(this.state.images);
+      this.forceUpdate();
+
+    if (this.state.clickedTwiceImages.includes(clickedImage))  {
+      console.log (" YOU LOOSE");     
+        var newState = {
+          score: 0,
+          clickedTwiceImages: [],
+          title: "You guessed incorrectly!"
+        }
+        this.setState(
+          newState
+        );
+      } else {
+        console.log ("you win")
+          this.state.clickedTwiceImages.push(clickedImage);
+          
+           newState = {
+              score: this.state.score+1,
+              title: "You guessed correctly!"
+            }
+            if (newState.score > this.state.topScore) {
+              newState.topScore = newState.score
+
+            }      
+            this.setState(
+                    newState
+            );
+      }
   }
 
   render() {
     const images = this.state.images.map ((image, i) => {
-      console.log("calling ");
      
       return (
       <div className="col s4 l3" key={i}>
       <div className="z-depth-5">
         <div className="card">
             <div className="card-image waves-effect waves-strong-effect" >
-              <img className="arrow" src = { image } 
-               onClick = {this.imageClick} alt =""></img>
+              <img className="arrow" value={ image } src = { image } 
+               onClick = {() => this.imageClick(image)} alt =""></img>
             </div>
           </div>
         </div>
@@ -46,19 +77,21 @@ class App extends Component {
         <div className="navbar-fixed">
           <nav>
             <div className="nav-wrapper black">
-              <div className="nav-content">
-                <ul className="tabs tabs-transparent">       
-                  <p className ="tab"><a className= "red-text darken-4" href="#test1">Clicky Game</a></p>
-                  <p className ="tab"><a className="active" href="#test2">Click an Image to Begin!</a></p>
-                  <p className ="tab"><a className= "active red-text darken-4" href="#test4">Score</a></p>          
+              <div className="row">
+                <ul className="tabs  tabs-transparent">
+                  <li className="tab col s4  red-text darken-4"><h5>Clicky Game</h5></li>
+                  <li className="tab col s4"><h5>{this.state.title}</h5></li>
+                  <li className="tab col s1 red-text darken-4">Score: {this.state.score}</li>
+                  <li className="tab col s1"> Top Score: {this.state.topScore}</li>
                 </ul>
-              </div>
+               </div>
             </div>   
-          </nav>
+         
+        </nav>
         </div>
    
         <div className="row">
-          <div className="card">
+          <div className="card ">
             <div className="card-image">
               <img src="/assets/images/disneybg.jpg" alt=""></img>
                 <div className="card-title">
